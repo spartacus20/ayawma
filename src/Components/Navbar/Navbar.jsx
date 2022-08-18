7f232daf9586315482fd6eaad4fcca5c292b102e
Spartacus20 <you@example.com>
2022-08-18 14:03:26 +0100

    Diseño de la Navbar al 70% lista

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,42 +1,64 @@
 import React, { useState } from 'react'
 import "./navbar.css"
 import { RiBarChartHorizontalFill } from 'react-icons/ri';
- 
-
+import { AiOutlineClose, AiOutlineUser} from 'react-icons/ai'; 
+import {BsHeadphones, BsSearch, BsCart} from 'react-icons/bs'; 
+import {GiProcessor} from 'react-icons/gi'; 
 function Navbar() {
 
   const [isActive, setActive] = useState(false); 
 
   const showSidebar = () => !setActive(!isActive); 
 
+  const [isUser, setUser] = useState(false); 
+  const showUserInfo = () => setUser(!isUser);
+
   return (
     <div className="NAVBAR">
       <div className="left">
-      <div className="icon1"> <RiBarChartHorizontalFill size={33}  onClick={showSidebar}/></div> 
-          <nav className={isActive ? 'nav-menu active': 'nav-menu'}>
-            <ul className='nav-menu-items' onClick={showSidebar}>
-              <li className="nav-icon">
-                
-              </li>
-              <li className="nav-text">
+        <div className="icon1"> <RiBarChartHorizontalFill size={40}  onClick={showSidebar}/> </div> 
+            <nav className={isActive ? 'nav-menu active': 'nav-menu'}>
+              <ul className='nav-menu-items' onClick={showSidebar}>
+                <li className="nav-icon">
+                  <AiOutlineClose  onClick={showSidebar} size={30}/>
+                  <a href="/">AYAWMA</a>
+                </li>
+                <li className="nav-text"></li>
+                <li className="nav-text">
+                    <GiProcessor/> <a href="">Componentes</a> 
+                  
+                </li>
+                <li className="nav-text">
+                <BsHeadphones/> <a href="">Periféricos</a> 
+                </li>
 
-              </li>
-              <li className="nav-text">
-                
-              </li>
-
-            </ul>
+              </ul>
 
 
 
-          </nav>
+            </nav>
+        <div className="BussinessName"><a href="/">AYAWMA</a></div>
       </div>
       <div className="middle">
-
+          <div className="SearchBar">
+  
+            <div className="SearchIcon"><BsSearch size={20}/></div>
+            <input type="text" name="searchbar" id="" className='bar'/>
+          </div>
+         
       </div>
       <div className="right">
-
-      </div>
+          <div className={isUser ? 'Usericon active': 'Usericon'}>
+            <AiOutlineUser onClick={showUserInfo} size={40}/>
+            <div className={isUser ? 'UserInfo active': 'UserInfo'}>
+              <a href="">Login</a>
+              <a href="">Register</a>
+            </div>
+          </div>
+          <div className="UserCart">
+            <BsCart size={40}/> 
+          </div>
+      </div>  
 
 
 
