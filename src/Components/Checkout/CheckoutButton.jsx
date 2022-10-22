92a44b40901d1ee082839379feeb370cdb14e5f2
JorgeDev <60814237+spartacus20@users.noreply.github.com>
2022-10-22 21:43:48 +0100

    Updates in design and CheckoutButton

--- src/Components/Checkout/CheckoutButton.jsx
+++ src/Components/Checkout/CheckoutButton.jsx
@@ -5,28 +5,32 @@ import Password from "../../Assets/Password";
 import { useStateValue } from '../../StateProvider'
 
 function CheckoutButton() {
-  
-  const [{basket}, dispatch] = useStateValue(); 
+
+  const [{ basket }, dispatch] = useStateValue();
   const handleCheckout = () => {
-    axios.post("/create-checkout-session", {
-       basket
-    }).then(res => {
-      if (res.data.url) {
-        window.location.href = res.data.url;
-      }
-    }).catch(err => {
-      console.log(err.message)
-    }  ); 
+
+    if(basket.length > 0){ 
+      axios.post("/create-checkout-session", {
+        basket
+      }).then(res => {
+        if (res.data.url) {
+          window.location.href = res.data.url;
+        }
+      }).catch(err => {
+        console.log(err.message)
+      });
+    }
+
   }
-  
-  
+
+
   return (
-    
+
     <>
       <button onClick={() => handleCheckout()} className="bg-black text-white  w-[200px] h-[50px] flex items-center mx-auto justify-center hover:bg-yellow-500"><Password /><span className="pl-2">Checkout</span></button>
 
     </>
-    )
+  )
 }
 
 export default CheckoutButton
--- src/Pages/SerachResullt/ProductResult.jsx
+++ src/Pages/SerachResullt/ProductResult.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./productresult.css"
+import { FiChevronDown } from "react-icons/fi"
 import { useParams } from "react-router-dom";
 import ProductContainer from "../../Components/ProductContainer/ProductContainer"
 import Navbar from "../../Components/Navbar/Navbar";
@@ -8,9 +9,13 @@ import axios from "../../Services/axios";
 function ProductResult() {
 
   const [data, setData] = useState([])
-  const [dropdown, setDropDown] = useState(true);
+  const [dropdown, setDropDown] = useState(false);
   let { product } = useParams();
-
+  const rotateIcon = () => {
+    setDropDown(!dropdown)
+    let element = document.getElementById("Icon");
+    element.classList.toggle("rotate");
+  }
   useEffect(() => {
 
     const test = async () => {
@@ -38,16 +43,24 @@ function ProductResult() {
       <div className="pageResult">
 
         <div className="Divider">
-          <span>find result "{product}"</span>
-          <div className="Filter">
-            <span>Filter</span>
-            <button></button>
+          <span>{data.length} results for "{product}"</span>
+          <div className="Filter" onClick={rotateIcon}>
+            <span>Sort by</span>
+            <div id="Icon">
+              <FiChevronDown size={30} />
+            </div>
+            <div className={dropdown? "Dropdown":"inactive"}>
+                <button className="Button">Featured</button>
+                <button className="Button">Newest</button>
+                <button className="Button">Price: High-Low</button>
+                <button className="Button">Price: Low-Hight</button>
+            </div>
           </div>
-       </div>
+        </div>
 
 
         <ProductContainer>
-         
+
           {/* I neeed to iterate with json file which get the database */}
           {data.map((e) => (<Card text={e.title} key={e.id} />))}
 
